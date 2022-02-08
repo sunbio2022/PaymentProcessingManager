8edf482c4ccf611d5728ebff0fd19053890358fd
Thirumal <thirumal@sunbioit.com>
2022-02-08 12:47:43 +0530

    Acquisiotion Model Created

---
+++ Model/AuthorizeStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentProcessingManager.Model
+{
+    public class AuthorizeStatus
+    {
+        [Key]
+        [Required]
+        public int AuthorizeStatusID { get; set; }
+        public string AuthorizeStatusName { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
