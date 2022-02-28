8521044b3e739371104d8f585acdeb9e68d3f5a8
kumar <kumar@karpov.co.in>
2022-02-28 10:12:21 +0530

    customer report

--- ClientApp/src/app/customer-report/customer-report.component.ts
+++ ClientApp/src/app/customer-report/customer-report.component.ts
@@ -12,12 +12,17 @@ import { Component, OnInit } from '@angular/core';
 
 export class CustomerReportComponent implements OnInit {
   public customers: Customer_Report[];
+  public customer: Customer[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     http.get<Customer_Report[]>('/api/Customer/GetCustomers').subscribe((data: any[]) => {
       console.log(data);
       this.customers = data;
     });
+    http.get<Customer[]>('/api/Customer/GetCustomers').subscribe(result => {
+      this.customer = result;
+      console.log(result);
+    }, error => console.error(error));
   }
 
   ngOnInit() {
@@ -33,5 +38,11 @@ interface Customer_Report {
   bank_account: number;
   dr_amount: number;
   cr_amount: number;
+}
+
+
+interface Customer {
+  customerID: number;
+  customerName: string;
 }
 
